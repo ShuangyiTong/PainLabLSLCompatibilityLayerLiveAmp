c78d57655c016c4579a6693bc853a7557e03ae7c
Shuangyi Tong <scat8460@ox.ac.uk>
2022-08-02 19:01:28 +0100

    implement triggers

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.IO.Ports;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,16 +18,18 @@ namespace PainLabDeviceLSLCompatialeLayer
         {
             var data_streams = ((LSLDataFrame)value).data_streams;
             writer.WriteStartObject();
-            for (int i = 0; i < data_streams.Length; i++)
+            for (int i = 0; i < data_streams[0].Length; i++)
             {
                 writer.WritePropertyName("Ch" + Convert.ToString(i + 1));
                 writer.WriteStartArray();
-                for (int j = 0; j < data_streams[i].Length; j++)
+                for (int j = 0; j < data_streams.Length; j++)
                 {
-                    writer.WriteValue(data_streams[i][j]);
+                    writer.WriteValue(data_streams[j][i]);
                 }
                 writer.WriteEndArray();
             }
+            writer.WritePropertyName("last_trigger_on_client");
+            writer.WriteValue(((LSLDataFrame)value).last_trigger_ts);
             writer.WriteEndObject();
         }
 
@@ -50,20 +53,39 @@ namespace PainLabDeviceLSLCompatialeLayer
     [JsonConverter(typeof(ChannelDataJsonConverter))]
     class LSLDataFrame
     {
-        public double[][] data_streams;
+        public float[][] data_streams;
+        public long last_trigger_ts;
 
-        public LSLDataFrame(float[][] sample)
+        public LSLDataFrame(float[][] sample, long last_ts)
         {
-            // Transpose operation
-            data_streams = new double[sample[0].Length][];
-            for (int i = 0; i < sample[0].Length; i++)
-            {
-                data_streams[i] = new double[sample.Length];
-                for (int j = 0; j < sample.Length; j++)
-                {
-                    data_streams[i][j] = (double)sample[j][i];
-                }
-            }
+            // will be transposed in json converter
+            data_streams = sample;
+            last_trigger_ts = last_ts;
+        }
+    }
+
+    [Serializable]
+    class LiveAmpControlFrame
+    {
+        public int trigger_channel = 0;
+        public long ApplyControlData(SerialPort sp)
+        {
+            Byte[] data = { (Byte)0 };
+
+            sp.Write(data, 0, 1);
+            Thread.Sleep(10);
+
+            DateTimeOffset now = DateTimeOffset.UtcNow;
+
+            data[0] = 0x01;
+            sp.Write(data, 0, 1);
+            Thread.Sleep(10);
+
+            data[0] = 0x00; 
+            sp.Write(data, 0, 1); 
+            Thread.Sleep(10);
+
+            return now.ToUnixTimeMilliseconds();
         }
     }
 
@@ -71,6 +93,8 @@ namespace PainLabDeviceLSLCompatialeLayer
     {
         static string descriptorPath = "Resources/device-descriptor.json";
         public StreamInfo info;
+        SerialPort sp;
+        long last_ts = -1;
         protected override void RegisterWithDescriptor()
         {
             string descriptorString = File.ReadAllText(descriptorPath);
@@ -83,21 +107,61 @@ namespace PainLabDeviceLSLCompatialeLayer
                 Jchannel_names.Add("Ch" + Convert.ToString(value), "float");
                 Jvisual_settings.Add("Ch" + Convert.ToString(value), "static");
             }
+            Jchannel_names.Add("last_trigger_on_client", "int");
 
             Jdescriptor.Add("data_to_report", Jchannel_names);
             Jdescriptor.Add("lsl_descriptor", info.as_xml());
             Jdescriptor.Add("visual_report", Jvisual_settings);
-            
+
             SendString(Jdescriptor.ToString());
 
             return;
         }
+
+        protected override void ApplyControlData()
+        {
+            LiveAmpControlFrame controlFrame
+                    = JsonConvert.DeserializeObject<LiveAmpControlFrame>
+                      (Encoding.UTF8.GetString(_controlBuffer, 0, (int)_numControlBytes));
+
+            last_ts = controlFrame.ApplyControlData(sp);
+        }
+
+        public void ControlApplicationThread()
+        {
+            while (true)
+            {
+                _waitOnControlSem.WaitOne();
+                HandlingControlData();
+            }
+        }
+
         public byte[] PrepareDataFrameBytes(float[][] sample)
         {
-            LSLDataFrame dataFrame = new LSLDataFrame(sample);
+            LSLDataFrame dataFrame = new LSLDataFrame(sample, last_ts);
             byte[] byteData = StringToBytes(JsonConvert.SerializeObject(dataFrame, Formatting.None));
 
             return byteData;
+        }
+
+        public void setupTriggerSerialPort()
+        {
+            sp = new SerialPort("COM7");
+            sp.Open();
+            sp.ReadTimeout = 5000;
+
+            // Attach the data received event handler
+            sp.DataReceived += TriggerBox_DataReceived;
+        }
+
+        static void TriggerBox_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            // get all available bytes from the input buffer
+            while (sp.BytesToRead > 0)
+            {
+                Console.WriteLine(sp.ReadByte());
+            }
         }
     }
     class Program
@@ -121,7 +185,11 @@ namespace PainLabDeviceLSLCompatialeLayer
 
             protocol.info = inlet.info();
 
-            protocol.Init(netConf);
+            protocol.Init(netConf, waitOnControl: true);
+            protocol.setupTriggerSerialPort();
+
+            Thread controlThread = new Thread(new ThreadStart(protocol.ControlApplicationThread));
+            controlThread.Start();
 
             float[][] sample = new float[subFramePerFrame][];
             for (int i = 0; i < subFramePerFrame; i++)
